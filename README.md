d23a62b2aa8ac77a819e62eec2a33056b5ab2ec9
Grollcake <grollcake@gmail.com>
2024-09-16 14:36:21 +0900

    updated

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 재사용 가능한 파일, 코드 모음
 
+## git clone
+
+```
+git clone https://github.com/grollcake/reusables.git
+```
+
 ### .bashrc 다운로드
 
+```
 wget https://raw.githubusercontent.com/grollcake/reusables/main/ubuntu/.bashrc -O ~/.bashrc
+```
 
 ### .dircolors 다운로드
 
+```
 wget https://raw.githubusercontent.com/grollcake/reusables/main/ubuntu/.dircolors -O ~/.dircolors
+```
---
+++ ubuntu/install-code-server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 스크립트가 sudo 또는 루트 권한으로 실행되었는지 확인
+if [ "$EUID" -eq 0 ]; then
+    echo "이 스크립트는 sudo 권한 없이 실행해야 합니다."
+    exit 1
+fi
+
+# 사용자 이름 입력 받기
+read -p "code-server에서 사용할 비밀번호를 입력하세요: " password
+
+# 사용자 이름이 비어있는지 확인
+if [[ -z "$password" ]]; then
+    echo "패스워드가 비어 있습니다."
+    return 1
+fi
+
+curl -fsSL https://code-server.dev/install.sh | sh
+sudo systemctl enable --now code-server@$USER
+sed -i "s/^bind-addr: .*/bind-addr: 0.0.0.0:8080/" ~/.config/code-server/config.yaml
+sed -i "s/^password: .*/password: $password/" ~/.config/code-server/config.yaml
+sudo systemctl restart code-server@$USER.service
+systemctl status code-server@$USER.service
+
+echo "code-server를 설치했습니다. http://$PUBLICIP:8080으로 접속하세요"
